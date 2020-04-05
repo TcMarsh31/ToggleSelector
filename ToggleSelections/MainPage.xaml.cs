42d35f90c5771fd6e2abe35b5deb0622a62718f8
panchanathantharsan <tharsan.3112@gmail.com>
2020-04-05 18:47:58 +0530

    Initial check-in of module ToggleSelections

---
+++ ToggleSelections/MainPage.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ToggleSelections
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        bool selection1Active = true;
+        bool selection2Active = false;
+        private double valueX, valueY;
+        private bool IsTurnX, IsTurnY;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            var x = e.TotalX; // TotalX Left/Right
+            var y = e.TotalY; // TotalY Up/Down
+
+            // StatusType
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    Debug.WriteLine("Started");
+                    break;
+                case GestureStatus.Running:
+                    Debug.WriteLine("Running");
+
+                    // Check that the movement is x or y
+                    if ((x >= 5 || x <= -5) && !IsTurnX && !IsTurnY)
+                    {
+                        IsTurnX = true;
+                    }
+
+                    if ((y >= 5 || y <= -5) && !IsTurnY && !IsTurnX)
+                    {
+                        IsTurnY = true;
+                    }
+
+                    // X (Horizontal)
+                    if (IsTurnX && !IsTurnY)
+                    {
+                        if (x <= valueX)
+                        {
+                            // Left
+                            if (selection2Active)
+                            {
+                                //make selection1 move to selection 2
+
+                                selection1Active = true;
+                                selection2Active = false;
+                                text2.TextColor = Color.Black;
+                                text1.TextColor = Color.White;
+                                runningFrame.TranslateTo(runningFrame.X,0,120);
+                                var duration = TimeSpan.FromSeconds(1);
+                                Vibration.Vibrate(duration);
+
+                            }
+                        }
+
+                        if (x >= valueX)
+                        {
+                            // Right
+                            if (selection1Active)
+                            {
+                                //make selection2 move to selection 1
+                                selection1Active = false;
+                                selection2Active = true;
+                                text2.TextColor = Color.White;
+                                text1.TextColor = Color.Black;
+                                runningFrame.TranslateTo(runningFrame.X+70, 0, 120);
+                                var duration = TimeSpan.FromSeconds(1);
+                                Vibration.Vibrate(duration);
+
+                            }
+                        }
+                    }
+
+
+                    break;
+                case GestureStatus.Completed:
+                    Debug.WriteLine("Completed");
+
+                    valueX = x;
+                    valueY = y;
+
+                    IsTurnX = false;
+                    IsTurnY = false;
+
+                    break;
+                case GestureStatus.Canceled:
+                    Debug.WriteLine("Canceled");
+                    break;
+
+
+            }
+        }
+
+        void OnText1Tapped(System.Object sender, System.EventArgs e)
+        {
+            if (selection2Active)
+            {
+                //make selection1 move to selection 2
+
+                selection1Active = true;
+                selection2Active = false;
+                text2.TextColor = Color.Black;
+                text1.TextColor = Color.White;
+                runningFrame.TranslateTo(runningFrame.X, 0, 120);
+                var duration = TimeSpan.FromSeconds(1);
+                Vibration.Vibrate(duration);
+            }
+        }
+
+        void OnText2Tapped(System.Object sender, System.EventArgs e)
+        {
+            if (selection1Active)
+            {
+                //make selection2 move to selection 1
+                selection1Active = false;
+                selection2Active = true;
+                text2.TextColor = Color.White;
+                text1.TextColor = Color.Black;
+                runningFrame.TranslateTo(runningFrame.X + 70, 0, 120);
+                var duration = TimeSpan.FromSeconds(1);
+                Vibration.Vibrate(duration);
+            }
+        }
+    }
+}
